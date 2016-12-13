bcbe0f76735934db04c7a3be4851607061eea614
Dave P <david.palmer@mariestopes.org.uk>
2016-12-13 14:51:28 +0000

    get our event info object ready to accept multiple prices

--- concert_item.py
+++ concert_item.py
@@ -19,14 +19,14 @@ class EventInfo(object):
     """ Full event info """
 
     def __init__(self, event_name="", link="", artist="", support="", city="", venue="",
-                 gig_date="", price=""):
+                 gig_date="", prices={}):
         # Details asked for in our exercise
         self._artist = artist
         self._support = support
         self._city = city
         self._venue = venue
         self._gig_date = gig_date
-        self._price = price
+        self._prices = prices
 
         # Other info that could be useful
         self._event_name = event_name
@@ -44,7 +44,7 @@ class EventInfo(object):
             'city': self._city,
             'venue': self._venue,
             'gig_date': self._gig_date,
-            'prices': self._price
+            'prices': self._prices
         }
 
     def __repr__(self):
